ff9d216fe02cc1720838d3b7a6e2341b32117c55
QingGo <zyqingjohn@qq.com>
2022-01-15 01:42:16 +0800

    update sql and makefile

--- makefile
+++ makefile
@@ -1,3 +1,7 @@
+.PHONY: test_sql
+test_sql:
+	bq query --use_legacy_sql=false `ggrep -v '\-\-' stat.sql`
+
 .PHONY: run_sql_example
 run_sql_example:
 	bq query --use_legacy_sql=false --format=csv `ggrep -v '\-\-' stat.sql` > results_example.csv
@@ -6,3 +10,24 @@ run_sql_example:
 # need a loog time to download
 run_sql_all:
 	bq query --use_legacy_sql=false --format=csv --max_rows=200000 "`ggrep -v '\-\-' stat.sql`" > results.csv
+
+.PHONY: deploy_db
+deploy_db: run_sql_all
+	scp ./results.csv zeng@49.232.3.102:~/ddnet_2022_chn_stat_deploy
+
+# need restart by hand
+# ps -aux | grep ddnet_2022_chn_stat_backend_linux64 | awk '{print $2}' | xargs kill
+# CSV_FILE_PATH=./results.csv nohup ./ddnet_2022_chn_stat_backend_linux64 >> ddnet_2022_chn_stat_backend_linux64.log 2>&1 &
+.PHONY: deploy_be 
+deploy_be:
+	cd ddnet_2022_chn_stat_backend; \
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ddnet_2022_chn_stat_backend_linux64 main.go; \
+	scp -r ./ddnet_2022_chn_stat_backend_linux64 zeng@49.232.3.102:~/ddnet_2022_chn_stat_deploy/
+	
+
+.PHONY: deploy_fe
+deploy_fe:
+	cd ddnet_2022_chn_stat_frontend; \
+	npm run build; \
+	npm run upload; \
+	scp -r ./build/index.html zeng@49.232.3.102:~/ddnet_2022_chn_stat_deploy/build/
--- stat.sql
+++ stat.sql
@@ -11,6 +11,26 @@ with ddnet_race_with_map_infos as (
         b.Stars as Stars,
         b.Mapper as Mapper,
         b.Timestamp as Timestamp_MAP,
+        EXTRACT(
+            DAYOFWEEK
+            FROM
+                a.Timestamp AT TIME ZONE "Asia/Shanghai"
+        ) as day_of_week,
+        EXTRACT(
+            HOUR
+            FROM
+                a.Timestamp AT TIME ZONE "Asia/Shanghai"
+        ) as hour,
+        EXTRACT(
+            MINUTE
+            FROM
+                a.Timestamp AT TIME ZONE "Asia/Shanghai"
+        ) as minute,
+        EXTRACT(
+            SECOND
+            FROM
+                a.Timestamp AT TIME ZONE "Asia/Shanghai"
+        ) as second,
         ROW_NUMBER() over (
             partition by a.Map,
             Name
@@ -41,6 +61,10 @@ ddnet_race_with_map_infos_2021 as (
         Stars,
         Mapper,
         Timestamp_MAP,
+        day_of_week,
+        hour,
+        minute,
+        second,
         ROW_NUMBER() over (
             partition by Map,
             Name
@@ -68,8 +92,8 @@ ddnet_race_with_map_infos_2021 as (
     from
         ddnet_race_with_map_infos
     where
-        Timestamp < '2022-01-01'
-        and Timestamp >= '2021-01-01'
+        Timestamp < '2022-01-01 08:00:00'
+        and Timestamp >= '2021-01-01 08:00:00'
 ),
 most_finished_map_2021_table as (
     select
@@ -106,210 +130,242 @@ most_finished_map_2021_table as (
         )
     where
         map_name_rank_finished_count_desc = 1
-), stat_with_repeat as (
+),
+stat_with_repeat as (
     select
-            Name,
-            count(Map) as finish_map_count,
-            count(distinct Map) as finish_distinct_map_count,
-            sum(Time / 3600) as finish_time_sum_hours,
-            sum(Points) as finish_points_total,
-            sum(
-                case
-                    when Server_Type = 'Fun' then Points
-                    else 0
-                end
-            ) as finish_fun_map_count,
-            sum(
-                case
-                    when Server_Type = 'Race' then Points
-                    else 0
-                end
-            ) as finish_race_map_count,
-            sum(
-                case
-                    when Server_Type = 'Solo' then Points
-                    else 0
-                end
-            ) as finish_solo_map_count,
-            sum(
-                case
-                    when Server_Type = 'DDmaX' then Points
-                    else 0
-                end
-            ) as finish_ddmax_map_count,
-            sum(
-                case
-                    when Server_Type = 'Dummy' then Points
-                    else 0
-                end
-            ) as finish_dummy_map_count,
-            sum(
-                case
-                    when Server_Type = 'Brutal' then Points
-                    else 0
-                end
-            ) as finish_brutal_map_count,
-            sum(
-                case
-                    when Server_Type = 'Insane' then Points
-                    else 0
-                end
-            ) as finish_insane_map_count,
-            sum(
-                case
-                    when Server_Type = 'Novice' then Points
-                    else 0
-                end
-            ) as finish_novice_map_count,
-            sum(
-                case
-                    when Server_Type = 'Moderate' then Points
-                    else 0
-                end
-            ) as finish_moderate_map_count,
-            sum(
-                case
-                    when Server_Type = 'Oldschool' then Points
-                    else 0
-                end
-            ) as finish_oldschool_map_count
-        from
-            ddnet_race_with_map_infos_2021
-        group by
-            Name
-), stat_2021_without_repeat as (
+        Name,
+        count(Map) as finish_map_count,
+        count(distinct Map) as finish_distinct_map_count,
+        sum(Time / 3600) as finish_time_sum_hours,
+        sum(Points) as finish_points_total,
+        sum(
+            case
+                when Server_Type = 'Fun' then Points
+                else 0
+            end
+        ) as finish_fun_map_count,
+        sum(
+            case
+                when Server_Type = 'Race' then Points
+                else 0
+            end
+        ) as finish_race_map_count,
+        sum(
+            case
+                when Server_Type = 'Solo' then Points
+                else 0
+            end
+        ) as finish_solo_map_count,
+        sum(
+            case
+                when Server_Type = 'DDmaX' then Points
+                else 0
+            end
+        ) as finish_ddmax_map_count,
+        sum(
+            case
+                when Server_Type = 'Dummy' then Points
+                else 0
+            end
+        ) as finish_dummy_map_count,
+        sum(
+            case
+                when Server_Type = 'Brutal' then Points
+                else 0
+            end
+        ) as finish_brutal_map_count,
+        sum(
+            case
+                when Server_Type = 'Insane' then Points
+                else 0
+            end
+        ) as finish_insane_map_count,
+        sum(
+            case
+                when Server_Type = 'Novice' then Points
+                else 0
+            end
+        ) as finish_novice_map_count,
+        sum(
+            case
+                when Server_Type = 'Moderate' then Points
+                else 0
+            end
+        ) as finish_moderate_map_count,
+        sum(
+            case
+                when Server_Type = 'Oldschool' then Points
+                else 0
+            end
+        ) as finish_oldschool_map_count
+    from
+        ddnet_race_with_map_infos_2021
+    group by
+        Name
+),
+stat_2021_without_repeat as (
     select
-            Name,
-            sum(Points) as total_points_earned,
-            sum(
-                case
-                    when Server_Type = 'Fun' then Points
-                    else 0
-                end
-            ) as finish_fun_point,
-            sum(
-                case
-                    when Server_Type = 'Race' then Points
-                    else 0
-                end
-            ) as finish_race_point,
-            sum(
-                case
-                    when Server_Type = 'Solo' then Points
-                    else 0
-                end
-            ) as finish_solo_point,
-            sum(
-                case
-                    when Server_Type = 'DDmaX' then Points
-                    else 0
-                end
-            ) as finish_ddmax_point,
-            sum(
-                case
-                    when Server_Type = 'Dummy' then Points
-                    else 0
-                end
-            ) as finish_dummy_point,
-            sum(
-                case
-                    when Server_Type = 'Brutal' then Points
-                    else 0
-                end
-            ) as finish_brutal_point,
-            sum(
-                case
-                    when Server_Type = 'Insane' then Points
-                    else 0
-                end
-            ) as finish_insane_point,
-            sum(
-                case
-                    when Server_Type = 'Novice' then Points
-                    else 0
-                end
-            ) as finish_novice_point,
-            sum(
-                case
-                    when Server_Type = 'Moderate' then Points
-                    else 0
-                end
-            ) as finish_moderate_point,
-            sum(
-                case
-                    when Server_Type = 'Oldschool' then Points
-                    else 0
-                end
-            ) as finish_oldschool_point,
-            sum(
-                case
-                    when Server_Type = 'Fun' then 1
-                    else 0
-                end
-            ) as finish_fun_new_map_count,
-            sum(
-                case
-                    when Server_Type = 'Race' then 1
-                    else 0
-                end
-            ) as finish_race_new_map_count,
-            sum(
-                case
-                    when Server_Type = 'Solo' then 1
-                    else 0
-                end
-            ) as finish_solo_new_map_count,
-            sum(
-                case
-                    when Server_Type = 'DDmaX' then 1
-                    else 0
-                end
-            ) as finish_ddmax_new_map_count,
-            sum(
-                case
-                    when Server_Type = 'Dummy' then 1
-                    else 0
-                end
-            ) as finish_dummy_new_map_count,
-            sum(
-                case
-                    when Server_Type = 'Brutal' then 1
-                    else 0
-                end
-            ) as finish_brutal_new_map_count,
-            sum(
-                case
-                    when Server_Type = 'Insane' then 1
-                    else 0
-                end
-            ) as finish_insane_new_map_count,
-            sum(
-                case
-                    when Server_Type = 'Novice' then 1
-                    else 0
-                end
-            ) as finish_novice_new_map_count,
-            sum(
-                case
-                    when Server_Type = 'Moderate' then 1
-                    else 0
-                end
-            ) as finish_moderate_new_map_count,
-            sum(
-                case
-                    when Server_Type = 'Oldschool' then 1
-                    else 0
-                end
-            ) as finish_oldschool_new_map_count
-        from
-            ddnet_race_with_map_infos_2021
-        where
-            map_name_rank_time_desc_2021 = 1
-        group by
-            Name
+        Name,
+        sum(Points) as total_points_earned,
+        sum(
+            case
+                when Server_Type = 'Fun' then Points
+                else 0
+            end
+        ) as finish_fun_point,
+        sum(
+            case
+                when Server_Type = 'Race' then Points
+                else 0
+            end
+        ) as finish_race_point,
+        sum(
+            case
+                when Server_Type = 'Solo' then Points
+                else 0
+            end
+        ) as finish_solo_point,
+        sum(
+            case
+                when Server_Type = 'DDmaX' then Points
+                else 0
+            end
+        ) as finish_ddmax_point,
+        sum(
+            case
+                when Server_Type = 'Dummy' then Points
+                else 0
+            end
+        ) as finish_dummy_point,
+        sum(
+            case
+                when Server_Type = 'Brutal' then Points
+                else 0
+            end
+        ) as finish_brutal_point,
+        sum(
+            case
+                when Server_Type = 'Insane' then Points
+                else 0
+            end
+        ) as finish_insane_point,
+        sum(
+            case
+                when Server_Type = 'Novice' then Points
+                else 0
+            end
+        ) as finish_novice_point,
+        sum(
+            case
+                when Server_Type = 'Moderate' then Points
+                else 0
+            end
+        ) as finish_moderate_point,
+        sum(
+            case
+                when Server_Type = 'Oldschool' then Points
+                else 0
+            end
+        ) as finish_oldschool_point,
+        sum(
+            case
+                when Server_Type = 'Fun' then 1
+                else 0
+            end
+        ) as finish_fun_new_map_count,
+        sum(
+            case
+                when Server_Type = 'Race' then 1
+                else 0
+            end
+        ) as finish_race_new_map_count,
+        sum(
+            case
+                when Server_Type = 'Solo' then 1
+                else 0
+            end
+        ) as finish_solo_new_map_count,
+        sum(
+            case
+                when Server_Type = 'DDmaX' then 1
+                else 0
+            end
+        ) as finish_ddmax_new_map_count,
+        sum(
+            case
+                when Server_Type = 'Dummy' then 1
+                else 0
+            end
+        ) as finish_dummy_new_map_count,
+        sum(
+            case
+                when Server_Type = 'Brutal' then 1
+                else 0
+            end
+        ) as finish_brutal_new_map_count,
+        sum(
+            case
+                when Server_Type = 'Insane' then 1
+                else 0
+            end
+        ) as finish_insane_new_map_count,
+        sum(
+            case
+                when Server_Type = 'Novice' then 1
+                else 0
+            end
+        ) as finish_novice_new_map_count,
+        sum(
+            case
+                when Server_Type = 'Moderate' then 1
+                else 0
+            end
+        ) as finish_moderate_new_map_count,
+        sum(
+            case
+                when Server_Type = 'Oldschool' then 1
+                else 0
+            end
+        ) as finish_oldschool_new_map_count
+    from
+        ddnet_race_with_map_infos_2021
+    where
+        map_name_rank_time_desc_2021 = 1
+    group by
+        Name
+),
+day_latest_finish_time_2021 as (
+    select
+        Name,
+        day_latest_finish_time,
+        day_latest_finish_map
+    from
+        (
+            select
+                Name,
+                FORMAT_TIMESTAMP("%Y-%m-%d %H:%M:%S", Timestamp, "Asia/Shanghai") as day_latest_finish_time,
+                Map as day_latest_finish_map,
+                ROW_NUMBER() over (
+                    partition by Name
+                    order by
+                        MOD((hour + 18), 24) desc,
+                        minute desc,
+                        second desc
+                ) as day_latest_finish_rank
+            from
+                ddnet_race_with_map_infos_2021
+        )
+    where
+        day_latest_finish_rank = 1
 )
+-- select
+--     *
+-- from
+--     day_latest_finish_time_2021
+-- limit
+--     100;
 -- maybe to-do
--- 最晚完成时间，什么时候算第二天，4 点？
+-- 最晚完成时间，什么时候算第二天，6 点？
 -- 最多组队的人，同伴名？
 select
     a.Name,
@@ -350,12 +406,13 @@ select
     finish_oldschool_map_count,
     c.most_finished_map_2021,
     c.map_finished_count,
-    c.most_finished_map_2021_spend_minues
+    c.most_finished_map_2021_spend_minues,
+    d.day_latest_finish_time,
+    d.day_latest_finish_map
 from
     stat_with_repeat a
-    join stat_2021_without_repeat b
-    on a.Name = b.Name
-    join most_finished_map_2021_table c
-    on a.Name = c.Name
+    join stat_2021_without_repeat b on a.Name = b.Name
+    join most_finished_map_2021_table c on a.Name = c.Name
+    join day_latest_finish_time_2021 d on a.Name = d.Name
 order by
     total_points_earned desc;
